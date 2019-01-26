11b87e766de38ae961297f98abc8b546e65db0c2
Charlie Azmitia <ccazmitia@gmail.com>
2019-01-26 16:32:38 -0500

    'added ls module'

--- bash.js
+++ bash.js
@@ -4,8 +4,15 @@ process.stdin.on('data', (data) =>{
     const cmd = data.toString().trim();
     process.stdout.write('You typed: ' + cmd + '\n');
     if(cmd === 'pwd'){
-        dirname = process.cwd();
-        process.stdout.write(dirname);
+    //    dirname = process.cwd();
+     //   process.stdout.write(dirname);
+     const pwd = require('./pwd');
+     pwd();
+    }
+
+    if(cmd === 'ls') {
+        const ls = require('./ls');
+        ls();
     }
 
 
